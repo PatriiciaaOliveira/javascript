8519effa5b73fc089fef2194b25de4b300f15944
PATRICIA RODRIGUES OLIVEIRA <157825841+PatriiciaaOliveira@users.noreply.github.com>
2024-02-10 13:51:22 -0300

    pronto

--- script.js
+++ script.js
@@ -5,13 +5,16 @@ function contar(){
     let res = document.getElementById('res')
 
     if(ini.value.length ==0 || fim.value.length == 0|| passo.value.length == 0) {
-        window.alert ('[ERRO] faltam dados!')
+        //window.alert ('[ERRO] faltam dados!')
         res.innerHTML = 'Impossível contar!'
     }else{
-        res.innerHTML='contando:'
+        res.innerHTML='contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if(p <= 0)
+        window.alert('Passo inválido! Passo 1')
+        p=1
         if (i<f){
             //contagem crescente
             for(let c = i; c <=f; c += p){
